6e2114b1f6ea08359255f9770d29b3e467e48b67
Nikesh Shrestha <nikesh@minerva.kgi.edu>
2020-05-02 16:06:47 -0700

    Added comments to init.py

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,13 @@ from flask_migrate import Migrate
 from flask_login import LoginManager
 
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
+app = Flask(__name__) #initiating flask app
+app.config.from_object(Config) # using the environment configuration to setup the app
+db = SQLAlchemy(app) # connecting database to app using SQL Alchemy
+migrate = Migrate(app, db) # migrating the new changes to database
+login = LoginManager(app) # Integrating LoginManager for login and authentication of user
 login.login_view = 'login'
-login.init_app(app)
+login.init_app(app) # Initializing the flask app that is integrated with LoginManager
 
+# Importing routes and models after initiating the apps.
 from app import routes, models
